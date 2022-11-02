247701011c6ea8caa940ec6d6333bb07382ccfe2
Roland Guijt <roland.guijt@gmail.com>
2022-11-02 10:27:47 +0000

    Add lab6 text

---
+++ Lab6/RocketLauncher/RocketLauncher/LaunchEngine.cs
@@ -0,0 +1,24 @@
+﻿using Launcher;
+using Launcher.Launchables;
+
+    public class LaunchEngine
+    {
+        private List<ILaunchable> _Launchables = new List<ILaunchable>();
+        
+        public int LaunchAll()
+        {
+            var duration = 0;
+            foreach (var launchable in _Launchables)
+            {
+                duration = duration + launchable.MissionDurationInHours;
+                launchable.Launch();
+            }
+            return duration;
+
+        }
+        public void QueueLaunchable(ILaunchable launchable)
+        {
+            _Launchables.Add(launchable);
+  
+        }
+    }
--- Lab6/RocketLauncher/RocketLauncher/Program.cs
+++ Lab6/RocketLauncher/RocketLauncher/Program.cs
@@ -1,12 +1,39 @@
-﻿namespace RocketLauncher
+﻿using Launcher.Launchables;
+using Launcher;
+
+namespace RocketLauncher
 {
     internal class Program
     { 
         static void Main(string[] args)
         {
             var launcher = new LaunchEngine();
-            launcher.QueueLaunchables();
-            launcher.LaunchAll();
+
+            Console.WriteLine("Press c to queue a career, r to queue a rocket, q to quit");
+            while (true)
+            {
+                var key = Console.ReadKey();
+                if (key.KeyChar == 'q')
+                    break;
+                if (key.KeyChar == 'r')
+                {
+                    var rocket = new Rocket();
+                    launcher.QueueLaunchable(rocket);
+                    Console.WriteLine($"{rocket.NameOfLaunchable} has been queued");
+                }
+                if (key.KeyChar == 'c')
+                {
+                    Console.WriteLine("Your name please");
+                    var name = Console.ReadLine();
+                    var career = new Career(name);
+                    launcher.QueueLaunchable(career);
+                    Console.WriteLine($"{career.NameOfLaunchable} has been queued");
+                }
+
+            }
+    
+            var duration = launcher.LaunchAll();
+            Console.WriteLine($"The total duration is {duration}");
         }
     }
 }
